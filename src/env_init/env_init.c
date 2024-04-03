60843f45b9acd9f749d0999747fa2690ba24b8bc
ael-mank <ael-mank@student.42.fr>
2024-04-03 08:22:52 +0200

    trying stuff

--- src/env_init/env_init.c
+++ src/env_init/env_init.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_cd.c                                            :+:      :+:    :+:   */
+/*   env_init.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/18 06:53:58 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/04/01 09:39:39 by ael-mank         ###   ########.fr       */
+/*   Created: 2024/03/30 07:54:47 by ael-mank          #+#    #+#             */
+/*   Updated: 2024/03/30 07:55:36 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_cd(char **args)
+int	init_env(t_env *env, char **og_env)
 {
-	if (args[1] == NULL)
-		fprintf(stderr, "ft_cd: expected argument\n");
-	else
-	{
-		if (chdir(args[1]) != 0)
-			perror("ft_cd");
-	}
-	return (1);
+	if 
 }
--- src/main.c
+++ src/main.c
@@ -6,44 +6,92 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 09:16:09 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/03/25 18:01:21 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/04/03 08:22:17 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//ADD = init env as linked lists
+// ADD = init env as linked lists
+
+int	check_exec_builtin(t_shell *shell)
+{
+	char **args;
+
+	args = ft_split(shell->line, ' ');
+	if (strcmp(args[0], "pwd") == 0)
+		ft_pwd();
+	else if (strcmp(args[0], "env") == 0)
+		print_env(shell->env);
+	else if (strcmp(args[0], "cd") == 0)
+		ft_cd(args);
+	else if (strcmp(args[0], "echo") == 0)
+		ft_echo(args);
+	else if (strcmp(args[0], "exit") == 0)
+	{
+		free(shell->line);
+		return (1);
+	}
+	else
+		return (1);
+	return (0);
+}
+
+//marche pas
+
+void exec_cmd(t_shell *shell)
+{
+    pid_t   pid;
+    int     status;
+    char    *argv[] = {shell->line, NULL};  // Construct the argv array
+
+    pid = fork();
+    printf("im in exec_cmd\n");
+    if (pid == 0)
+    {
+        execve(shell->line, argv, shell->env);
+        perror("execve");
+        exit(1);
+    }
+    else if (pid < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+}
+
+void	mini_loop(t_shell *shell)
+{
+	while (1)
+	{
+		shell->line = readline("minishell> ");
+		if (!shell->line)
+			break ;
+		if (check_syntax_errors(shell->line))
+		{
+			free(shell->line);
+			continue;
+		}
+		add_history(shell->line);
+		if (check_exec_builtin(shell))
+			exec_cmd(shell);
+		free(shell->line);
+	}
+}
 
 int	main(int argc, char **argv, char **env)
 {
+	t_shell *shell;
+
 	(void)argc;
 	(void)argv;
-	(void)env;
+	shell = malloc(sizeof(t_shell));
+	shell->env = env;
 	setup_signals();
-	while (1)
-	{
-		char *line = readline("minishell> ");
-		if (!line)
-			break ;
-		add_history(line);
-		check_syntax_errors(line);
-		//Add check for builtin function
-		// if (strcmp(line, "pwd") == 0)
-		// 	ft_pwd();
-		// else if (strcmp(line, "env") == 0)
-		// 	print_env(env);
-		// else if (strcmp(line, "cd") == 0)
-		// 	ft_cd();
-		// else if (strcmp(line, "echo") == 0)
-		// 	ft_echo(NULL);
-		// else if (strcmp(line, "exit") == 0)
-		// {
-		// 	free(line);
-		// 	break ;
-		// }
-		// else
-		// 	printf("Error: command not found: %s\n", line);
-		free(line);
-	}
+	mini_loop(shell);
 	return (0);
 }
