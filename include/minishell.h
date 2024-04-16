308648f241de936a5d3384f27bd1c53bcd07e9c5
ael-mank <ael-mank@student.42.fr>
2024-04-16 07:36:35 +0200

    adding homework

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 09:36:17 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/04/01 09:28:12 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/04/05 07:58:10 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,13 @@ void		increment_quote_count(char c, int *single_quote_count,
 				int *double_quote_count);
 const char	*skip_whitespace(const char *input);
 int			is_invalid_redirection(const char **input);
+
+// Exec
+void exec_cmd(t_shell *shell);
+int	check_exec_builtin(t_shell *shell);
+
+//signals
+
+void	sigint_handler(int sig);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,63 +6,11 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 09:16:09 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/04/03 08:22:17 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/04/05 07:38:03 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// ADD = init env as linked lists
-
-int	check_exec_builtin(t_shell *shell)
-{
-	char **args;
-
-	args = ft_split(shell->line, ' ');
-	if (strcmp(args[0], "pwd") == 0)
-		ft_pwd();
-	else if (strcmp(args[0], "env") == 0)
-		print_env(shell->env);
-	else if (strcmp(args[0], "cd") == 0)
-		ft_cd(args);
-	else if (strcmp(args[0], "echo") == 0)
-		ft_echo(args);
-	else if (strcmp(args[0], "exit") == 0)
-	{
-		free(shell->line);
-		return (1);
-	}
-	else
-		return (1);
-	return (0);
-}
-
-//marche pas
-
-void exec_cmd(t_shell *shell)
-{
-    pid_t   pid;
-    int     status;
-    char    *argv[] = {shell->line, NULL};  // Construct the argv array
-
-    pid = fork();
-    printf("im in exec_cmd\n");
-    if (pid == 0)
-    {
-        execve(shell->line, argv, shell->env);
-        perror("execve");
-        exit(1);
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
-}
 
 void	mini_loop(t_shell *shell)
 {
