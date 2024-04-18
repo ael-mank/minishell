d81b83ecdf476516bc78eb850410a680a3aaca0b
Yif <yrigny@student.42.fr>
2024-04-18 14:11:21 +0200

    added shell_routine

--- src/main.c
+++ src/main.c
@@ -12,41 +12,52 @@
 
 #include "minishell.h"
 
-int	mini_loop(t_shell *shell)
+void	shell_routine(void)
 {
+	char	*line;
+	t_token	*tokens;
+
 	while (1)
 	{
-		shell->line = readline("minishell> ");
-		if (!shell->line)
-			return(0);
-		if (ft_strlen(shell->line) == 0)
-		{
-			free(shell->line);
-			continue;
-		}
-		if (check_syntax_errors(shell->line))
-		{
-			free(shell->line);
+		if (GNL == 0)
+			line = readline("minishell> ");
+		else
+			line = get_next_line(0);
+		if (!line)
+			break ;
+		if (empty_line(line))
 			continue ;
-		}
-		add_history(shell->line);
-		if (check_exec_builtin(shell))
-			exec_cmd(shell);
-		free(shell->line);
+		add_history(line);
+		tokens = check_syntax_and_tokenize(line);
+		// if (!tokens)
+		// 	g_signal = 258;
+		if (!tokens)
+			continue ;
+		parse_tokens(tokens);
+		free_cmd_list();
+		// if (!g_signal)
+		// {
+		// 	exec_list = parse_tokens(&tokens);
+		// 	execution_manager(exec_list, env, &status);
+		// 	free_exec_list(exec_list);
+		// }
+		// update_env_status(env, status, "=?");
 	}
-	return(1);
+	rl_clear_history();
 }
 
-int	main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **envp)
 {
-	t_shell *shell;
+	t_ms *ms;
 
 	(void)argc;
 	(void)argv;
-	shell = malloc(sizeof(t_shell));
-	init_env(env, shell);
-	setup_signals();
-	if(!mini_loop(shell))
-		ft_exit(shell, 0);
-	return (0);
+	ms = get_ms();
+	// g_signal = 0;
+	if (init_env(envp, ms) == FAILURE)
+		return (FAILURE);
+	// ms->curr_dir = get_curr_dir();
+	shell_routine();
+	free_env(ms->env);
+	return (SUCCESS);
 }
