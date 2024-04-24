72cb4ea82640f4d0b9e9a519ecda5de5ddd699ec
Amine <ael-mank@student.42.fr>
2024-04-24 12:46:37 +0200

    fixed echo

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 07:38:11 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/04/18 15:20:25 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/04/24 12:35:15 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	check_exec_builtin(t_ms *shell)
 
 	if (!args)
 		return (1);
-	if (strcmp(args[0], "pwd") == 0)
+	if (ft_strncmp("pwd", args[0], 3) == 0)
 		ft_pwd();
-	else if (strcmp(args[0], "env") == 0)
+	else if (ft_strncmp("env", args[0], 3) == 0)
 		print_env(shell->env);
-	else if (strcmp(args[0], "cd") == 0)
+	else if (ft_strncmp("cd", args[0], 2) == 0)
 		ft_cd(args, shell->env);
-	else if (strcmp(args[0], "echo") == 0)
+	else if (ft_strncmp("echo", args[0], 4) == 0)
 		ft_echo(args);
-	else if (strcmp(args[0], "export") == 0)
+	else if (ft_strncmp("export", args[0], 6) == 0)
 		ft_export(args, shell->env);
-	else if (strcmp(args[0], "exit") == 0)
+	else if (ft_strncmp("exit", args[0], 4) == 0)
 		ft_exit(shell);
 	else
 		return (1);
--- src/main.c
+++ src/main.c
@@ -6,46 +6,41 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 09:16:09 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/04/18 15:58:01 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/04/22 10:44:26 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	shell_routine(void)
+void shell_routine(void)
 {
-	char	*line;
-	t_token	*tokens;
+    char *line;
+    t_token *tokens;
 
-	while (1)
-	{
-		char *(user) = match_env_var("USER", 4);
-		char *(pwd) = getcwd(NULL, 0);
-		char *(prompt) = ft_strjoin(user, "@");
-		prompt = ft_strjoin(prompt, pwd);
-		prompt = ft_strjoin(prompt, " $ ");
-		line = readline(prompt);
-		if (!line)
-			break ;
-		if (empty_line(line))
-			continue ;
-		add_history(line);
-		tokens = check_syntax_and_tokenize(line);
-		// if (!tokens)
-		// 	g_signal = 258;
-		if (!tokens)
-			continue ;
-		parse_tokens(tokens);
-		free_cmd_list();
-		// if (!g_signal)
-		// {
-		// 	exec_list = parse_tokens(&tokens);
-		// 	execution_manager(exec_list, env, &status);
-		// 	free_exec_list(exec_list);
-		// }
-		// update_env_status(env, status, "=?");
-	}
-	rl_clear_history();
+    while (1)
+    {
+        char *user = match_env_var("USER", 4);
+        char *pwd = getcwd(NULL, 0);
+        char *prompt = ft_strjoin(user, "@");
+        char *tempPrompt = ft_strjoin(prompt, pwd);
+        free(prompt);
+        free(pwd);
+        char *finalPrompt = ft_strjoin(tempPrompt, " $ ");
+        free(tempPrompt);
+        line = readline(finalPrompt);
+        free(finalPrompt);
+        if (!line)
+            break;
+        if (empty_line(line))
+            continue;
+        add_history(line);
+        tokens = check_syntax_and_tokenize(line);
+        if (!tokens)
+            continue;
+        parse_tokens(tokens);
+        free_cmd_list();
+    }
+    rl_clear_history();
 }
 
 int	main(int argc, char **argv, char **envp)
