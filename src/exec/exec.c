3a68393a9acc2d9192526cb32a4d165ad5100ba2
Amine El Mankouri <ael-mank@paul-f4Ar4s4.clusters.42paris.fr>
2024-05-06 10:00:40 +0200

    refixed prompt leaks and norm

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 16:23:39 by yrigny            #+#    #+#             */
-/*   Updated: 2024/05/03 11:34:16 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/05/06 09:56:46 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,29 +78,4 @@ int	exec_builtin(t_cmd *child)
 	if (child->fd_out != STDOUT_FILENO)
 		dup2(original, STDOUT_FILENO);
 	return (0 || exit_code);
-}
-
-int	exec_single_builtin(t_cmd *cmd)
-{
-	char	*cmd_name;
-	t_ms	*ms;
-	int		exit_code;
-
-	cmd_name = cmd->cmd_arr[0];
-	ms = get_ms();
-	if (!ft_strncmp(cmd_name, "echo", 5))
-		exit_code = ft_echo(cmd->cmd_arr);
-	else if (!ft_strncmp(cmd_name, "cd", 3))
-		exit_code = ft_cd(cmd->cmd_arr, ms->env);
-	else if (!ft_strncmp(cmd_name, "pwd", 4))
-		exit_code = ft_pwd();
-	else if (!ft_strncmp(cmd_name, "export", 7))
-		exit_code = ft_export(cmd->cmd_arr, ms->env);
-	else if (!ft_strncmp(cmd_name, "unset", 6))
-		exit_code = ft_unset(cmd->cmd_arr, ms->env);
-	else if (!ft_strncmp(cmd_name, "env", 4))
-		exit_code = print_env(ms->env);
-	else if (!ft_strncmp(cmd_name, "exit", 5))
-		exit_code = ft_exit(cmd->cmd_arr);
-	return (exit_code);
 }
