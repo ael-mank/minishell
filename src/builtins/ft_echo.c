4b9e37f383e10fb3a761356dcf127039235850f2
Amine El Mankouri <ael-mank@paul-f4Ar1s2.clusters.42paris.fr>
2024-05-07 09:50:05 +0200

    fixed echo

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ael-mank <ael-mank@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 06:49:21 by ael-mank          #+#    #+#             */
-/*   Updated: 2024/05/03 11:05:12 by ael-mank         ###   ########.fr       */
+/*   Updated: 2024/05/07 09:49:17 by ael-mank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,24 @@
 
 int	ft_echo(char **args)
 {
-	int	i;
-	int	n_flag;
+    int	i;
+    int	n_flag;
 
-	i = 1;
-	n_flag = 0;
-	if (!args[1])
-		return (0);
-	if (args[i] && ft_strncmp("-n", args[i], 3) == 0)
-	{
-		n_flag = 1;
-		i++;
-	}
-	while (args[i])
-	{
-		printf("%s", args[i]);
-		if (args[i + 1])
-			printf(" ");
-		i++;
-	}
-	if (!n_flag)
-		printf("\n");
-	return (0);
+    i = 1;
+    n_flag = 0;
+    while (args[i] && ft_strncmp("-n", args[i], 3) == 0)
+    {
+        n_flag = 1;
+        i++;
+    }
+    while (args[i])
+    {
+        printf("%s", args[i]);
+        if (args[i + 1])
+            printf(" ");
+        i++;
+    }
+    if (!n_flag)
+        printf("\n");
+    return (0);
 }
